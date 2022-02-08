602e930c6cf3e6fbeceaea3a8fe9979f4216d921
Muhammed Shittu <coolzyte@gmail.com>
2022-02-08 17:11:39 +0100

    rapidapi implemented homepage and cryptocurrencies page done

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "e4fd62ae24msh3c6af816a710b19p180df3jsnfaf3fc21990d",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+export const { useGetCryptosQuery } = cryptoApi;
