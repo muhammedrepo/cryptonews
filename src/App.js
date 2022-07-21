dc1f7ac66c7dd2eb82a60817e6e582d480c1d614
Muhammed Shittu <coolzyte@gmail.com>
2022-07-21 15:28:38 +0100

    fix: url redirect

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <div className="routes">
             <Routes>
               <Route path="/" element={<Homepage />} />
-              <Route path="/exchanges" element={<Exchanges />} />
+              {/* <Route path="/exchanges" element={<Exchanges />} /> */}
               <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
               <Route path="/crypto/:coinId" element={<CryptoDetails />} />
               <Route path="/news" element={<News />} />
